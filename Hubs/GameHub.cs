7ca719747add30d52dcb3da6a63ea21537923639
karabomatabane <karaoramakau@gmail.com>
2024-06-12 08:50:41 +0200

    Feature: Add create and join buttons, and styling.

---
+++ Hubs/GameHub.cs
@@ -0,0 +1,17 @@
+﻿using Crazy8.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Crazy8Web.Hubs;
+
+public class GameHub : Hub
+{
+    public async Task ShowFaceUp(Card card)
+    {
+        await Clients.All.SendAsync("FaceUp", card);
+    }
+
+    public async Task ShowTurn(string playerId)
+    {
+        await Clients.All.SendAsync("PlayerTurn", playerId);
+    }
+}
